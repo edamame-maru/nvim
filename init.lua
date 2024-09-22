ba0c9aa2b13956f80ed1d7d78d4492ea1af157f4
edamame-maru <edamame-maru@localhost.localdomain>
2024-09-22 19:33:39 +0100

    updated theme

--- init.lua
+++ init.lua
@@ -11,3 +11,14 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.background = 'dark'
 vim.opt.cursorline = true
+
+vim.cmd[[colorscheme solarized]]
+
+vim.o.background = 'dark' -- or 'light'
+vim.g.solarized_italic_comments = true
+vim.g.solarized_italic_keywords = true
+vim.g.solarized_italic_functions = true
+vim.g.solarized_italic_variables = false
+vim.g.solarized_contrast = true
+vim.g.solarized_borders = false
+vim.g.solarized_disable_background = false
