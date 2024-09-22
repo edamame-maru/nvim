b8773a070e439b63293acb8f2a8e8a0ab047b3b2
edamame-maru <edamame-maru@localhost.localdomain>
2024-09-22 15:26:16 +0100

    initial commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.background = 'dark'
+vim.opt.cursorline = true
