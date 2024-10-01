21a1a5df720bcada01eb6bacafeb4cdd454fa118
edamame-maru <119129834+edamame-maru@users.noreply.github.com>
2024-10-01 20:29:32 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,36 +4,20 @@ vim.opt.number = true
 -- Enable 24-bit RGB color in the TUI
 vim.opt.termguicolors = true
 
-
--- Set background (uncomment one of these lines)
--- vim.opt.background = "dark"
--- vim.opt.background = "light"
-vim.cmd[[colo slate]]
-
--- Enable line numbers
-vim.opt.number = true
-
--- Enable 24-bit RGB color in the TUI
-vim.opt.termguicolors = true
-
--- Set background (uncomment one of these lines)
--- vim.opt.background = "dark"
--- vim.opt.background = "light"
-
 -- Set colorscheme to slate
-vim.cmd[[colo slate]]
+vim.cmd[[colorscheme slate]]
 
 -- Create an event handler for the FileType autocommand
 vim.api.nvim_create_autocmd('FileType', {
-  -- This handler will fire when the buffer's 'filetype' is "rust"
   pattern = 'rust',
-  callback = function(args)
-    vim.lsp.start({
-      name = 'rust-analyzer',
-      cmd = {'rust-analyzer'},
-      -- Set the "root directory" to the parent directory of the file in the
-      -- current buffer that contains a "Cargo.toml" file.
-      root_dir = vim.fs.dirname(vim.fs.find({'Cargo.toml'}, { upward = true })[1]),
-    })
+  callback = function()
+    local root_dir = vim.fs.dirname(vim.fs.find({'Cargo.toml'}, { upward = true })[1])
+    if root_dir then
+      vim.lsp.start({
+        name = 'rust-analyzer',
+        cmd = {'rust-analyzer'},
+        root_dir = root_dir,
+      })
+    end
   end,
 })
