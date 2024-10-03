f9197276dd13a82d7fd37c777362d1661f3e6c16
edamame-maru <so1maru.ueda@gmail.com>
2024-10-03 20:09:25 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install ale
+mkdir -p ~/.local/share/nvim/site/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.local/share/nvim/site/pack/git-plugins/start/ale
