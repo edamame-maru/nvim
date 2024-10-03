ab2465bf771010cbab593fe6ae1be64eaa5f34e6
edamame-maru <119129834+edamame-maru@users.noreply.github.com>
2024-10-03 21:13:30 +0100

    Add files via upload

--- init.lua
+++ init.lua
@@ -19,3 +19,19 @@ vim.call('plug#end')
 -- Set colorscheme
 vim.cmd('colorscheme kanagawa')
 
+-- Make sure to set up ALE
+vim.g.ale_linters = {
+    rust = {'rust-analyzer'},
+}
+
+vim.g.ale_fixers = {
+    rust = {'rustfmt'},
+}
+
+-- Enable ALE
+vim.g.ale_enabled = 1
+vim.g.ale_lint_on_text_changed = 'always'
+vim.g.ale_lint_on_insert_leave = 1
+
+-- Additional configurations for rust-analyzer
+vim.g.ale_rust_rust_analyzer_executable = 'rust-analyzer' -- Adjust the path if necessary
