5a194631ba932085bab9fdaa93f78ab62e22fd8e
edamame-maru <so1maru.ueda@gmail.com>
2024-10-09 21:11:43 +0100

    updated status bar

--- init.lua
+++ init.lua
@@ -41,7 +41,7 @@ require('lualine').setup {
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
 
-    lualine_x = {'encoding', 'filetype'},
+    lualine_x = {'encoding', 'filetype', 'hostname'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
