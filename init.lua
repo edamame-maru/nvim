5818c10d1a05c0c7e8d1e4da69013f00ac90fe72
edamame-maru <so1maru.ueda@gmail.com>
2024-10-10 20:53:52 +0100

    updated

--- init.lua
+++ init.lua
@@ -1,58 +1,58 @@
--- Enable line numbers
-vim.opt.number = true
-
--- Enable 24-bit RGB color in the TUI
-vim.opt.termguicolors = true
-
--- Show cursorline
-vim.opt.cursorline = true
+vim.opt.number = true             -- Enable line numbers
+vim.opt.termguicolors = true      -- Enable 24-bit RGB color in the TUI
+vim.opt.cursorline = true	  -- Show cursorline
+vim.opt.expandtab = true          -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4            -- Shift 4 spaces when tab
+vim.opt.tabstop = 4               -- 1 tab = 4 spaces
+vim.opt.smartindent = true        -- Smart indentation
+vim.opt.wrap = false              -- Disable line wrapping
+vim.opt.termguicolors = true      -- Enable true colors
 
 -- Plugins
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.vim/plugged')
 
 -- Themes
-Plug 'rebelot/kanagawa.nvim'           -- Kanagawa theme
-Plug('dracula/vim', { as = 'dracula' })  -- Dracula theme
-Plug 'gbprod/nord.nvim' -- Nord theme
-Plug 'maxmx03/solarized.nvim' -- Solarized theme
+Plug 'folke/tokyonight.nvim'
 
 -- Useful plugins
 Plug 'jiangmiao/auto-pairs'             -- Auto-Pairs
 Plug 'rust-lang/rust.vim'               -- Rust.vim
-Plug 'nvim-lualine/lualine.nvim' -- Status bar
-Plug 'mhinz/vim-startify' -- Startify
-
+Plug 'nvim-lualine/lualine.nvim'        -- Lualine status bar
+Plug 'nvim-tree/nvim-web-devicons'      -- Additional icons for lualine
+Plug('neoclide/coc.nvim', {branch = 'release'})  -- Conquer of Completion
 
 vim.call('plug#end')
 
 -- Set colorscheme
-vim.cmd('colorscheme dracula')
+vim.cmd('colorscheme tokyonight')
 
--- Lualine setup
 require('lualine').setup {
-  options = {
-    theme = 'dracula',  -- You can set the theme to 'dracula' or another if you prefer
-    section_separators = '',  -- Customize separators if needed
-    component_separators = '',
-    icons_enabled = true
-  },
+    options = {
+        theme = 'tokyonight', -- You can choose your preferred theme
+        section_separators = '',
+        component_separators = '',
+    },
 
     sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
+        lualine_x = {'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'},
+    },
 
-    lualine_x = {'encoding', 'filetype', 'hostname'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {},
+    },
+
+    tabline = {},
+    extensions = {},
 }
 
--- Set up lists
-vim.g.startify_lists = {
-    { type = 'files', header = {'   Recent Files'} },
-    { type = 'dir', header = {'   Current Directory'} },
-}
-
-vim.g.startify_session_persistence = 1  -- Enable session persistence
